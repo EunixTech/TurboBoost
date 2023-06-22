fae8c8bb34e836c9d905e818e50efadb109307e5
themanmohan <kdilmohan101@gmail.com>
2023-06-22 17:35:33 +0530

    feat: adding session and helmet

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-
 require(`dotenv`).config({ path: `process.env` });
 
 const express = require(`express`),
     cors = require('cors'),
+    helmet = require("helmet"),
     mongoose = require("mongoose"),
+    expressSession = require(`express-session`),
+    MongoStore = require(`connect-mongo`),
     app = express();
 
 
-const dbConnection = require('./config/dbConnection');
+const dbConnection = require('./config/dbConnection'),
+    loadHelmet = require(`./loaders/helmets`),
+    loadExpressSession = require(`./loaders/expressSession`);
+
+loadHelmet(app, helmet);
+loadExpressSession(app, expressSession, MongoStore);
+
+// body parser
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
 dbConnection(mongoose)
 
 app.use(cors());
+
 
 app.use((err, req, res, next) => {
     console.log(err)
