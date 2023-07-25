bcef9a0eb48f54277d6a36f9daa59b6063dc1465
themanmohan <gitmail223344@gmail.com>
2023-07-25 22:10:08 +0530

    feat: adding method for losslessCompression

--- resources/image-compression.js
+++ resources/image-compression.js
@@ -4,16 +4,26 @@ const losslessCompression = async(imageBuffer, sharp) =>{
           .webp({ lossless: true })
           .toBuffer();
    
-        console.log(
-          'Lossless Compression completed and saved as "lossless_compressed_image.webp".'
-        );
+        console.log('Lossless Compression completed and saved as "lossless_compressed_image.webp".');
         return compressedImageBuffer
       } catch (error) {
         console.error("Error during Lossless Compression:", error.message);
       }
 }
 
+const lossyCompression = async(imageBuffer, sharp) =>{
+    try {
+        const compressedImageBuffer = await sharp(imageBuffer)
+          .jpeg({ quality: 80 })
+          .toBuffer();
+        // console.log('Lossy Compression completed and saved as "lossy_compressed_image.jpg".');
+        return compressedImageBuffer;
+    } catch (error) {
+        console.error("Error during Lossy Compression:", error.message);
+    }
+}
 
 module.exports ={
-    losslessCompression
+    losslessCompression,
+    lossyCompression
 }
