e3839cc452ef1aba14509e409898c34615206a94
themanmohan <gitmail223344@gmail.com>
2023-08-07 23:25:23 +0530

    adding script for  setting Cache-Control Headers

--- test/cachingScript.js
+++ test/cachingScript.js
@@ -35,57 +35,164 @@
 
   
   // eslint-disable-next-line no-unused-vars
-  function refreshDynamicImages(html) {
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(html, 'text/html');
-    var dynamicImages = doc.querySelectorAll('img.dynamic-image');
+//   function refreshDynamicImages(html) {
+//     var parser = new DOMParser();
+//     var doc = parser.parseFromString(html, 'text/html');
+//     var dynamicImages = doc.querySelectorAll('img.dynamic-image');
   
-    dynamicImages.forEach(function(img) {
-      var imageUrl = img.getAttribute('src');
-      var updatedAt = img.getAttribute('data-updated-at');
-      var optimizedImageUrl = imageUrl + '?v=' + updatedAt;
+//     dynamicImages.forEach(function(img) {
+//       var imageUrl = img.getAttribute('src');
+//       var updatedAt = img.getAttribute('data-updated-at');
+//       var optimizedImageUrl = imageUrl + '?v=' + updatedAt;
   
-      img.setAttribute('src', optimizedImageUrl);
-    });
+//       img.setAttribute('src', optimizedImageUrl);
+//     });
   
-    return doc.documentElement.outerHTML;
-  }
+//     return doc.documentElement.outerHTML;
+//   }
   
  
 
+// //   function applyAggressiveCaching(html) {
+// //     // Define the version timestamp
+// //     var version = new Date().getTime();
+  
+// //     // Regular expression pattern to match asset URLs in HTML
+// //     var assetPattern = /(<link[^>]+href="[^"]+\.(?:css|js)[^"]*"[^>]*>)/gi;
+  
+// //     // Replace asset URLs with versioned URLs
+// //     var versionedHTML = html.replace(assetPattern, function(match, p1) {
+// //       return p1.replace(/(href="[^"]+\.(css|js)[^"]*")/, '$1?v=' + version);
+// //     });
+  
+// //     return versionedHTML;
+// //   }
+  
+// //   // Example HTML content
+// //   var exampleHTML = `
+// //   <!DOCTYPE html>
+// //   <html>
+// //   <head>
+// //     <link rel="stylesheet" href="styles.css">
+// //     <script src="script.js"></script>
+// //   </head>
+// //   <body>
+// //     <!-- ... -->
+// //   </body>
+// //   </html>
+// //   `;
+  
+// //   // Apply aggressive caching to the example HTML
+// //   var cachedHTML = applyAggressiveCaching(exampleHTML);
+  
+// //   console.log(cachedHTML); // Output the modified HTML with versioned asset URLs
+  
+  
 
-  function applyAggressiveCaching(html) {
-    // Define the version timestamp
-    var version = new Date().getTime();
+// // function setCacheControlForHTML(htmlContent, cacheDuration) {
+// //     // Create a new Response object with the modified Cache-Control header
+// //     const modifiedResponse = new Response(htmlContent, {
+// //       headers: { 'Cache-Control': `max-age=${cacheDuration}` }
+// //     });
   
-    // Regular expression pattern to match asset URLs in HTML
-    var assetPattern = /(<link[^>]+href="[^"]+\.(?:css|js)[^"]*"[^>]*>)/gi;
+// //     // Return the modified HTML content as a promise
+// //     return modifiedResponse.text();
+// //   }
   
-    // Replace asset URLs with versioned URLs
-    var versionedHTML = html.replace(assetPattern, function(match, p1) {
-      return p1.replace(/(href="[^"]+\.(css|js)[^"]*")/, '$1?v=' + version);
-    });
+// //   // Example usage
+// //   const htmlContent = `
+// //     <!DOCTYPE html>
+// //     <html>
+// //     <head>
+// //       <!-- ... other meta tags and styles ... -->
+// //     </head>
+// //     <body>
+// //       <!-- ... body content ... -->
+// //     </body>
+// //     </html>
+// //   `;
   
-    return versionedHTML;
-  }
+// //   const cacheDurationInSeconds = 3600; // Set the desired cache duration in seconds
   
-  // Example HTML content
-  var exampleHTML = `
-  <!DOCTYPE html>
+// //   // Set Cache-Control headers for the HTML content
+// //   setCacheControlForHTML(htmlContent, cacheDurationInSeconds)
+// //     .then(modifiedHtmlContent => {
+// //       // Use the modified HTML content as needed
+// //       console.log(modifiedHtmlContent);
+// //     })
+// //     .catch(error => {
+// //       console.error('Error setting Cache-Control headers:', error);
+// //     });
+  
+//     function setCacheControlHeaders(htmlContent) {
+//         // Parse the HTML content as a DOM object
+//         const parser = new DOMParser();
+//         const doc = parser.parseFromString(htmlContent, 'text/html');
+      
+//         // Define the asset URLs and corresponding cache settings
+//         const assets = [
+//           { url: 'styles.css', maxAge: 604800 }, // Example CSS asset
+//           // Add more assets and settings as needed
+//         ];
+      
+//         // Loop through each asset and update its cache settings
+//         assets.forEach(asset => {
+//           const assetElement = doc.querySelector(`link[href*="${asset.url}"]`);
+//           if (assetElement) {
+//             // Append the Cache-Control header attribute to the asset's <link> tag
+//             assetElement.setAttribute('data-cache-control', `max-age=${asset.maxAge}`);
+//           }
+//         });
+      
+//         // Serialize the modified DOM back to an HTML string
+//         const updatedHtmlContent = new XMLSerializer().serializeToString(doc);
+//         return updatedHtmlContent;
+//       }
+      
+//       // Example usage
+//       const originalHtml = `
+//         <html>
+//           <head>
+//             <link rel="stylesheet" href="styles.css">
+//           </head>
+//           <body>
+//             <h1>Hello, world!</h1>
+//           </body>
+//         </html>
+//       `;
+      
+//       const updatedHtml = setCacheControlHeaders(originalHtml);
+//       console.log(updatedHtml);
+      
+
+const cheerio = require('cheerio');
+
+function setCacheControlHeaders(htmlContent) {
+  const $ = cheerio.load(htmlContent);
+
+  const assets = [
+    { selector: 'link[href*="styles.css"]', maxAge: 604800 },
+    // Add more asset selectors and settings as needed
+  ];
+
+  assets.forEach(asset => {
+    $(asset.selector).attr('data-cache-control', `max-age=${asset.maxAge}`);
+  });
+
+  return $.html();
+}
+
+// Example usage
+const originalHtml = `
   <html>
-  <head>
-    <link rel="stylesheet" href="styles.css">
-    <script src="script.js"></script>
-  </head>
-  <body>
-    <!-- ... -->
-  </body>
+    <head>
+      <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+      <h1>Hello, world!</h1>
+    </body>
   </html>
-  `;
-  
-  // Apply aggressive caching to the example HTML
-  var cachedHTML = applyAggressiveCaching(exampleHTML);
-  
-  console.log(cachedHTML); // Output the modified HTML with versioned asset URLs
-  
-  
+`;
+
+const updatedHtml = setCacheControlHeaders(originalHtml);
+console.log(updatedHtml);
