cb8085351b5ff8eb24225114d9b71d829eddef1f
themanmohan <gitmail223344@gmail.com>
2023-08-11 17:11:22 +0530

    feat: Adding script that take css content and update @font-face propeties with font-display: swap

---
+++ resources/scripts/add-font-display.js
@@ -0,0 +1,25 @@
+const AddingFontDisplayInCss = (cssContent) => {
+
+    const fontFaceRegex = /@font-face\s*\{[\s\S]*?\}/g,
+        fontFaceMatches = cssContent.match(fontFaceRegex);
+
+    if (fontFaceMatches && fontFaceMatches.length > 0) {
+
+        const updatedCss = cssContent.replace(fontFaceRegex, (match) => {
+
+            // Check if font-display property already exists
+            if (match.includes("font-display"))return match;
+
+            // Add font-display property
+            return match.replace("}", "font-display: swap;}");
+
+        });
+
+        return updatedCss;
+    }
+
+  return cssContent;
+
+};
+
+module.exports = AddingFontDisplayInCss;
