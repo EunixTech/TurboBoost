3ef046e2d9b66f03b1f97c1738b47486e98da167
themanmohan <gitmail223344@gmail.com>
2023-08-16 18:44:18 +0530

    feat: adding script to add google tag manager in liquid them

--- resources/scripts/check-google-tag-manager.js
+++ resources/scripts/check-google-tag-manager.js
@@ -1,19 +1,7 @@
-const gtmScriptPattern = /<script async>\(function\(w,d,s,l,i\){w\[l\]=w\[l\]||\[\];w\[l\].push\({'gtm\.start':\s*new Date\(\).getTime\(\),event:'gtm\.js'\}\);var f=d\.getElementsByTagName\(s\)\[0\],\s*j=d\.createElement\(s\),dl=l!='dataLayer'?'&l='+l:'';j\.async=true;j\.src=\s*'https:\/\/www\.googletagmanager\.com\/gtm\.js\?id='+i+dl;f\.parentNode\.insertBefore\(j,f\);\s*}\)\(window,document,'script','dataLayer','[A-Z0-9-]+'\);<\/script>/;
+const regrex = /<script async>\(function\(w,d,s,l,i\){w\[l\]=w\[l\]||\[\];w\[l\].push\({'gtm\.start':\s*new Date\(\).getTime\(\),event:'gtm\.js'\}\);var f=d\.getElementsByTagName\(s\)\[0\],\s*j=d\.createElement\(s\),dl=l!='dataLayer'?'&l='+l:'';j\.async=true;j\.src=\s*'https:\/\/www\.googletagmanager\.com\/gtm\.js\?id='+i+dl;f\.parentNode\.insertBefore\(j,f\);\s*}\)\(window,document,'script','dataLayer','[A-Z0-9-]+'\);<\/script>/;
 
-function checkForGoogleTagManager(html) {
-  return gtmScriptPattern.test(html);
+const  checkForGoogleTagManager = (htmlContent) => {
+  return regrex.test(htmlContent);
 }
 
-// Example HTML
-const htmlContent = `
-  <!-- ... your HTML content ... -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KSN6KLJG')</script>
-  <!-- ... more HTML content ... -->
-`;
-
-const containsGTM = checkForGoogleTagManager(htmlContent);
-console.log('Google Tag Manager is present:', containsGTM);
+module.exports = checkForGoogleTagManager
