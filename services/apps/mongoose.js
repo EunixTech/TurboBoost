5ea1b3f8ad5431ac0c3963ee34c278a5d17b1a0c
themanmohan <gitmail223344@gmail.com>
2023-08-21 23:32:09 +0530

    refactor; adding error handler:

--- services/apps/mongoose.js
+++ services/apps/mongoose.js
@@ -4,58 +4,65 @@ const {
 } = require("../../handlers/jsonResponseHandlers");
 
 class MongooseAPI {
-   
-    async createOperation(model, dataObj, res ) {
+    
+    async handleError(e, res) {
+        console.error(e);
+        return sendFailureJSONResponse(res, { message: e.message });
+    }
+
+    async createOperation(model, dataObj, res) {
         try {
             const createdItem = await model.create(dataObj);
-
-            if (!createdItem) { 
+            if (!createdItem) {
                 return sendFailureJSONResponse(res, { message: "Failed to create item" });
-            } else {
-                return sendSuccessJSONResponse(res, {
-                    message: "Item created successfully",
-                    status: 201,
-                    data: createdItem
-                });
             }
+            return sendSuccessJSONResponse(res, {
+                message: "Item created successfully",
+                status: 201,
+                data: createdItem
+            });
         } catch (e) {
-            console.error(e); // Log the error for debugging
-            return sendFailureJSONResponse(res, { message: e.message });
+            return this.handleError(e, res);
         }
-
     }
 
     async readOperation(model, queryObj, res) {
         try {
-            const item = await model.find(queryObj);
+            const item = await model.findOne(queryObj);
+            if (!item) {
+                return sendFailureJSONResponse(res, { message: "Item not found" });
+            }
             return sendSuccessJSONResponse(res, { data: item });
         } catch (e) {
-            console.error(e);
-            return sendFailureJSONResponse(res, { message: e.message });
+            return this.handleError(e, res);
         }
     }
 
     async updateOperation(model, queryObj, updateObj, res) {
         try {
             const updatedItem = await model.findOneAndUpdate(queryObj, updateObj, { new: true });
+            if (!updatedItem) {
+                return sendFailureJSONResponse(res, { message: "Failed to update item" });
+            }
             return sendSuccessJSONResponse(res, { data: updatedItem });
         } catch (e) {
-            console.error(e);
-            return sendFailureJSONResponse(res, { message: e.message });
+            return this.handleError(e, res);
         }
     }
 
     async deleteOperation(model, queryObj, res) {
         try {
-            await model.findOneAndDelete(queryObj);
+            const deletedItem = await model.findOneAndDelete(queryObj);
+            if (!deletedItem) {
+                return sendFailureJSONResponse(res, { message: "Failed to delete item" });
+            }
             return sendSuccessJSONResponse(res, { message: "Item deleted successfully" });
         } catch (e) {
-            console.error(e);
-            return sendFailureJSONResponse(res, { message: e.message });
+            return this.handleError(e, res);
         }
     }
-
-    
 }
+
+module.exports = MongooseAPI;
 
 module.exports = MongooseAPI;
