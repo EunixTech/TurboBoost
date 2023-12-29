e7010c2a1ec972f6f2d7846fc95c4acf4c55c1ca
themanmohan <gitmail223344@gmail.com>
2023-12-29 17:19:16 +0530

    fix: fixing app uninstall issue

--- controllers/shopify.js
+++ controllers/shopify.js
@@ -986,9 +986,10 @@ async function criticalCssRestore(shopifyAdmin, redisStore) {
 
 exports.appUninstallation = (req, res) =>{
  
-        console.log()
+      
          const { id, name, api_client_id, shop_id, domain } = req.body;
-         console.log("uninstall hook working",api_client_id, name)
+         console.log("uninstall hook working",api_client_id, name);
+         console.log(req.body)
         res.json("working");
 
 
